adc9f0b5d517b9d85563d637f9278ec6110cc9e0
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-10-16 16:38:57 -0500

    joined tables to find username of the commentor, removed some of table in comments, added check in delete to make sure if someone deletes a shelter that are authroized

--- routes/shelters.js
+++ routes/shelters.js
@@ -6,25 +6,28 @@ const db = require("../models/shelters.js")
 router.post('/', restricted, (req, res) => {
 
     const shelter =  req.body;
+    if (req.body.username !== 'admin') {
+        res.status(400).json({ message: "You are not permitted to make this request."})
+    } else {
+        let clean = {
+            name: (shelter.name) ? shelter.name : null,
+            street_num: (shelter.street_num) ? shelter.street_num : null,
+            road: (shelter.road) ? shelter.road : null,
+            city: (shelter.city) ? shelter.city : null,
+            stateAbbrev: shelter.stateAbbrev,
+            zip_code: (shelter.zip_code) ? shelter.zip_code : null,
+            description: (shelter.description) ? shelter.description : null
+        }
 
-    let clean = {
-        name: (shelter.name) ? shelter.name : null,
-        street_num: (shelter.street_num) ? shelter.street_num : null,
-        road: (shelter.road) ? shelter.road : null,
-        city: (shelter.city) ? shelter.city : null,
-        stateAbbrev: shelter.stateAbbrev,
-        zip_code: (shelter.zip_code) ? shelter.zip_code : null,
-        description: (shelter.description) ? shelter.description : null
+        db
+        .add(clean)
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Internal Server Error"})
+        })
     }
-
-    db
-    .add(clean)
-    .then(result => {
-        res.status(200).json(result)
-    })
-    .catch(err => {
-        res.status(500).json({ message: "Internal Server Error"})
-    })
 })
 
 router.get('/', (req, res) => {
@@ -52,8 +55,13 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.delete('/:id', restricted, (req, res) => {
+router.delete('/:id/:username', restricted, (req, res) => {
     const id = req.params.id;
+    const username = req.params.username;
+
+    if (username !== 'admin') {
+        res.status(400).json({ message: "You are not permitted to make this request."})
+    }
 
     db
     .remove(id)
