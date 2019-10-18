b96f24f8ff8cead42a6a87ab5146126bf17af67a
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-10-18 16:42:29 -0500

    moved order by

--- models/comments.js
+++ models/comments.js
@@ -9,7 +9,7 @@ module.exports = {
 }
 
 function findById(id) {
-    return db('comments').where({ shelter_id: id}).join('users', 'comments.user_id', '=', 'users.id').select('users.username', 'comments.id', 'comments.shelter_id', 'comments.posted_at', 'comments.comment').orderBy(['comments.id', 'desc']);
+    return db('comments').where({ shelter_id: id}).orderBy('comments.id', 'desc').join('users', 'comments.user_id', '=', 'users.id').select('users.username', 'comments.id', 'comments.shelter_id', 'comments.posted_at', 'comments.comment');
 }
 
 async function addComment(obj) {
