0428a408a77cea0a395252c82b8d79055abbc801
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-10-23 11:58:44 -0500

    routes for adding user feedback complete

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ server.use(cors());
 const login = require('./routes/login');
 const shelters = require('./routes/shelters');
 const comments = require('./routes/comments');
+const tellUs = require('./routes/tellUs');
+
 
 server.use('/login', login);
 server.use('/shelters', shelters);
 server.use('/comments', comments);
+server.use('./tellus', tellUs);
 
 module.exports = server;
