42c1c304fdf2546eceadd0e0e8fb45019aeac6a1
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-10-23 12:34:58 -0500

    fixed seeds issue

--- database/seeds/tellUs.js
+++ database/seeds/tellUs.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
   return knex('tellus').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('tellus').insert([
         {id: 1, shelterName: 'Shelter1', address: "5 street", yourName: "Todd", contactNum: "555-555-5555", info: "Here is some info."},
         {id: 2, shelterName: 'Shelter2', address: "6 street", yourName: "Sarah", contactNum: "555-555-5555", info: "some more info"},
         {id: 3, shelterName: 'Shelter3', address: "7 street", yourName: "AJ", contactNum: "555-555-5555", info: "even more info."},
