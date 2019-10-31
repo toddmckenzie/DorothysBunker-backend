ee1f8468341595a0146c0a74c028973c0ea13f28
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-10-31 16:57:55 -0500

    changes to verify

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ const login = require('./routes/login');
 const shelters = require('./routes/shelters');
 const comments = require('./routes/comments');
 const tellUs = require('./routes/tellUs');
+const verify = require('./routes/verify');
 
 
 server.use('/login', login);
 server.use('/shelters', shelters);
 server.use('/comments', comments);
 server.use('/tellus', tellUs);
+server.use('/verify', verify);
 
 module.exports = server;
