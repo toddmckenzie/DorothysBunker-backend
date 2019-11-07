89c0b8361f47ea073bfdbfb3faeca488196edb3a
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-11-06 18:18:42 -0600

    changes to post/delete to decode token and check to make sure priviledge is granted to do so

--- routes/shelters.js
+++ routes/shelters.js
@@ -3,32 +3,35 @@ const restricted = require('../auth/restricted.js')
 const db = require("../models/shelters.js")
 
 
-router.post('/', restricted, (req, res) => {
-
+router.post('/', (req, res) => {
+    const token = req.headers.authorization;
     const shelter =  req.body;
-    if (req.body.username !== 'admin') {
-        res.status(400).json({ message: "You are not permitted to make this request."})
-    } else {
-        let clean = {
-            name: (shelter.name) ? shelter.name : null,
-            street_num: (shelter.street_num) ? shelter.street_num : null,
-            road: (shelter.road) ? shelter.road : null,
-            city: (shelter.city) ? shelter.city : null,
-            stateAbbrev: shelter.stateAbbrev,
-            zip_code: (shelter.zip_code) ? shelter.zip_code : null,
-            description: (shelter.description) ? shelter.description : null
-        }
-
-        db
-        .add(clean)
-        .then(result => {
-            res.status(200).json(result)
-        })
-        .catch(err => {
-            res.status(500).json({ message: "Internal Server Error"})
-        })
+    let clean = {
+        name: (shelter.name) ? shelter.name : null,
+        street_num: (shelter.street_num) ? shelter.street_num : null,
+        road: (shelter.road) ? shelter.road : null,
+        city: (shelter.city) ? shelter.city : null,
+        stateAbbrev: shelter.stateAbbrev,
+        zip_code: (shelter.zip_code) ? shelter.zip_code : null,
+        description: (shelter.description) ? shelter.description : null
     }
-})
+    if (token) {
+        jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json(err)
+            } else {
+               if (decodedToken.username === "adminTodd"){
+                   db
+                    .add(clean)
+                    .then(result => {
+                        res.status(200).json(result)
+                    })
+                    .catch(err => {
+                        res.status(500).json({ message: "Internal Server Error"})
+                    })
+               }
+        }})
+    }})
 
 router.get('/', (req, res) => {
 
@@ -55,23 +58,27 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.delete('/:id/:username', restricted, (req, res) => {
+router.delete('/:id', restricted, (req, res) => {
+
     const id = req.params.id;
-    const username = req.params.username;
-
-    if (username !== 'admin') {
-        res.status(400).json({ message: "You are not permitted to make this request."})
-    }
-
-    db
-    .remove(id)
-    .then(result => {
-        res.status(200).json(result)
-    })
-    .catch(err => {
-        res.status(500).json({ message: "Internal Server Error"})
-    })
-})
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json(err)
+            } else {
+               if (decodedToken.username === "adminTodd"){
+                   db
+                    .remove(id)
+                    .then(result => {
+                        res.status(200).json(result)
+                    })
+                    .catch(err => {
+                        res.status(500).json({ message: "Internal Server Error"})
+                    })
+               }
+        }})
+    }})
 
 module.exports = router;
 
